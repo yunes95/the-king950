934a91d59d2cf37f2276c5a45efc8cc93e4df3ed
yunes95 <31017364+yunes95@users.noreply.github.com>
2018-05-14 17:58:51 +0200

    Create main.js

---
+++ .gitignore/main.js
@@ -0,0 +1,495 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+var prefix = ("!");
+
+bot.on('ready', function () {
+    bot.user.setPresence({game:{name:'ACTIVER',type: 0}})
+  console.log("Je suis connecté !")
+  });
+bot.login('NDQ0ODE3MTMyMDkwMjk0MzAz.Ddjj2g.mxS1sUXMNgkB1l-QEnqsAPuxRJA');
+
+bot.on('message', message => {
+    if (message.content === 'salut THE KING') {
+      message.reply('Salut comment va tu ?!')
+    }
+
+    if (message.content === prefix + "help"){
+      var help_embed = new Discord.RichEmbed()
+      .setColor('#01FE09')
+      .addField("Commande du bot !", "  -/help : Affiche les commande de The King!")
+      //message.channel.sendMessage("Voici les commande de The King :\n -/help pour afficher les commande")
+      console.log("Commande Help demandée!")
+    }
+  });
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  bot.on('message', message => {
+    if (message.content === 'salut the king') {
+      message.reply('Salut comment va tu ?!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'Salut the king') {
+      message.reply('Salut comment va tu ?!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'Salut THE KING') {
+      message.reply('Salut comment va tu ?!')
+    }
+  });
+
+  bot.on('message', message => {
+    if (message.content === 'slt THE KING') {
+      message.reply('Salut comment va tu ?!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'slt the king') {
+      message.reply('Salut comment va tu ?!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'Slt THE KING') {
+      message.reply('Salut comment va tu ?!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'Slt the king') {
+      message.reply('Salut comment va tu ?!')
+    }
+  });
+
+  bot.on('message', message => {
+    if (message.content === 'Sava et toi') {
+      message.reply('Sava Merci de demandé!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'bien et toi?') {
+      message.reply('Sava Merci de demandé!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'Bien et toi?') {
+      message.reply('Sava Merci de demandé!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'bien et toi') {
+      message.reply('Sava Merci de demandé!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'Bien et toi') {
+      message.reply('Sava Merci de demandé!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'Sava et toi?') {
+      message.reply('Sava Merci de demandé!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'sava et toi?') {
+      message.reply('Sava Merci de demandé!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'Qui est le boss ici?') {
+      message.reply('Lorenzo puie l Empereur Yunes et burgeur frite et enfin cyril!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'Qui est le boss ici') {
+      message.reply('Lorenzo puie l Empereur Yunes et burgeur frite et enfin cyril!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'Qui est le boss icing?') {
+      message.reply('Lorenzo puie l Empereur Yunes et burgeur frite et enfin cyril!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'Qui est le boss icing') {
+      message.reply('Lorenzo puie l Empereur Yunes et burgeur frite et enfin cyril!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'Qui est le boss icing') {
+      message.reply('Lorenzo puie l Empereur Yunes et Burgeur frite et enfin cyril!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'Avec Yunes ...') {
+      message.reply('Avec Yunes on dort moin bête!')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'Une souris verte') {
+      message.reply('Une souris verte Qui courait dans l herbe Je l attrape par la queue Je la montre à ces messieurs Ces messieurs me disent Trempez-la dans l huile Trempez-la dans l eau Ça fera un escargot Tout chaud Je la mets dans un tiroir Elle me dit qu il fait trop noir Je la mets dans mon chapeau Elle me dit qu il fait trop chaud Je la mets dans ma culotte Elle me fait trois petites crottes')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'Bonne nuit THE KING') {
+      message.reply('Merci bonne a toi aussi !')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'bonne nuit THE KING') {
+      message.reply('Merci bonne a toi aussi !')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'bonne-nuit THE KING') {
+      message.reply('Merci bonne a toi aussi !')
+    }
+  });
+  bot.on('message', message => {
+    if (message.content === 'bonne-nuit THE KING') {
+      message.reply('Merci bonne a toi aussi !')
+    }
+  });
+  bot.on('message',message => {
+    if (message.content === 'Qu est que tu sais fair THE KING?') {
+  message.reply('Je sais te parlé pour le moment ces tout!')
+  }
+});
+bot.on('message',message => {
+  if (message.content === 'Qu est que tu sais fair the king?') {
+message.reply('Je sais te parlé pour le moment ces tout!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Qu est que tu sais faire theking?') {
+message.reply('Je sais te parlé pour le moment ces tout!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Theking?') {
+message.reply('Oui que puie-je fair pour vous?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Theking ?') {
+message.reply('Oui que puie-je fair pour vous?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'The king?') {
+message.reply('Oui que puie-je fair pour vous?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'The king ?') {
+message.reply('Oui que puie-je fair pour vous?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'THE KING?') {
+message.reply('Oui que puie-je fair pour vous?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'THE KING ?') {
+message.reply('Oui que puie-je fair pour vous?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'the king?') {
+message.reply('Oui que puie-je fair pour vous?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'the king ?') {
+message.reply('Oui que puie-je fair pour vous?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'The king comment on devien modo?') {
+message.reply('Pour devenir modérateur vous devais remplire ce questionnaire https://docs.google.com/forms/d/e/1FAIpQLScqLt-1pUbp7IvJlIlA46mi0EpYs2aMbBvhHTqQ6FvredF1gQ/viewform?usp=pp_url ?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'THE KING comment on devien modo?') {
+message.reply('Pour devenir modérateur vous devais remplire ce questionnaire https://docs.google.com/forms/d/e/1FAIpQLScqLt-1pUbp7IvJlIlA46mi0EpYs2aMbBvhHTqQ6FvredF1gQ/viewform?usp=pp_url ?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'the king comment on devien modo?') {
+message.reply('Pour devenir modérateur vous devais remplire Ce questionnaire https://docs.google.com/forms/d/e/1FAIpQLScqLt-1pUbp7IvJlIlA46mi0EpYs2aMbBvhHTqQ6FvredF1gQ/viewform?usp=pp_url ?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'the king comment on devien modo?') {
+message.reply('Pour devenir modérateur vous devais remplire Ce questionnaire https://docs.google.com/forms/d/e/1FAIpQLScqLt-1pUbp7IvJlIlA46mi0EpYs2aMbBvhHTqQ6FvredF1gQ/viewform?usp=pp_url ?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'The king comment on devien modo') {
+message.reply('Pour devenir modérateur vous devais remplire Ce questionnaire https://docs.google.com/forms/d/e/1FAIpQLScqLt-1pUbp7IvJlIlA46mi0EpYs2aMbBvhHTqQ6FvredF1gQ/viewform?usp=pp_url ?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'the king comment on devien modo') {
+message.reply('Pour devenir modérateur vous devais remplire Ce questionnaire https://docs.google.com/forms/d/e/1FAIpQLScqLt-1pUbp7IvJlIlA46mi0EpYs2aMbBvhHTqQ6FvredF1gQ/viewform?usp=pp_url ?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'THE KING comment on devien modo?') {
+message.reply('Pour devenir modérateur vous devais remplire Ce questionnaire https://docs.google.com/forms/d/e/1FAIpQLScqLt-1pUbp7IvJlIlA46mi0EpYs2aMbBvhHTqQ6FvredF1gQ/viewform?usp=pp_url ?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'The king comment on devien modo?') {
+message.reply('Pour devenir modérateur vous devais remplire ce questionnaire https://docs.google.com/forms/d/e/1FAIpQLScqLt-1pUbp7IvJlIlA46mi0EpYs2aMbBvhHTqQ6FvredF1gQ/viewform?usp=pp_url ?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'THE KING comment on devien modo?') {
+message.reply('Pour devenir modérateur vous devais remplire ce questionnaire https://docs.google.com/forms/d/e/1FAIpQLScqLt-1pUbp7IvJlIlA46mi0EpYs2aMbBvhHTqQ6FvredF1gQ/viewform?usp=pp_url ?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Comment on devien modo?') {
+message.reply('Pour devenir modérateur vous devais remplire Ce questionnaire https://docs.google.com/forms/d/e/1FAIpQLScqLt-1pUbp7IvJlIlA46mi0EpYs2aMbBvhHTqQ6FvredF1gQ/viewform?usp=pp_url ?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'comment on devien modo?') {
+message.reply('Pour devenir modérateur vous devais remplire Ce questionnaire https://docs.google.com/forms/d/e/1FAIpQLScqLt-1pUbp7IvJlIlA46mi0EpYs2aMbBvhHTqQ6FvredF1gQ/viewform?usp=pp_url ?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'comment on devien modo') {
+message.reply('Pour devenir modérateur vous devais remplire Ce questionnaire https://docs.google.com/forms/d/e/1FAIpQLScqLt-1pUbp7IvJlIlA46mi0EpYs2aMbBvhHTqQ6FvredF1gQ/viewform?usp=pp_url ?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Comment on devien modo') {
+message.reply('Pour devenir modérateur vous devais remplire Ce questionnaire https://docs.google.com/forms/d/e/1FAIpQLScqLt-1pUbp7IvJlIlA46mi0EpYs2aMbBvhHTqQ6FvredF1gQ/viewform?usp=pp_url ?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Comment on devien modo?') {
+message.reply('Pour devenir modérateur vous devais remplire Ce questionnaire https://docs.google.com/forms/d/e/1FAIpQLScqLt-1pUbp7IvJlIlA46mi0EpYs2aMbBvhHTqQ6FvredF1gQ/viewform?usp=pp_url ?!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Comment on joue avec Loren?') {
+message.reply('Pour jouer avec Lorenzo il faut remplire ce Questionnaire https://docs.google.com/forms/d/e/1FAIpQLSf0AduITS6vIDJQOo9rOo9RZv6Hr_FasY5KX1A-76-3el4ujg/viewform!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Comment on joue avec Loren ?') {
+message.reply('Pour jouer avec Lorenzo il faut remplire ce Questionnaire https://docs.google.com/forms/d/e/1FAIpQLSf0AduITS6vIDJQOo9rOo9RZv6Hr_FasY5KX1A-76-3el4ujg/viewform!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Comment on joue avec Loren') {
+message.reply('Pour jouer avec Lorenzo il faut remplire ce Questionnaire https://docs.google.com/forms/d/e/1FAIpQLSf0AduITS6vIDJQOo9rOo9RZv6Hr_FasY5KX1A-76-3el4ujg/viewform!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Comment on joue avec Lorenzo?') {
+message.reply('Pour jouer avec Lorenzo il faut remplire ce Questionnaire https://docs.google.com/forms/d/e/1FAIpQLSf0AduITS6vIDJQOo9rOo9RZv6Hr_FasY5KX1A-76-3el4ujg/viewform!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Comment on joue avec Lorenzo ?') {
+message.reply('Pour jouer avec Lorenzo il faut remplire ce Questionnaire https://docs.google.com/forms/d/e/1FAIpQLSf0AduITS6vIDJQOo9rOo9RZv6Hr_FasY5KX1A-76-3el4ujg/viewform!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Comment on joue avec Lorenzo') {
+message.reply('Pour jouer avec Lorenzo il faut remplire ce Questionnaire https://docs.google.com/forms/d/e/1FAIpQLSf0AduITS6vIDJQOo9rOo9RZv6Hr_FasY5KX1A-76-3el4ujg/viewform!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'The king comment on joue avec Loren?') {
+message.reply('Pour jouer avec Lorenzo il faut remplire ce Questionnaire https://docs.google.com/forms/d/e/1FAIpQLSf0AduITS6vIDJQOo9rOo9RZv6Hr_FasY5KX1A-76-3el4ujg/viewform!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'The king comment on joue avec Loren') {
+message.reply('Pour jouer avec Lorenzo il faut remplire ce Questionnaire https://docs.google.com/forms/d/e/1FAIpQLSf0AduITS6vIDJQOo9rOo9RZv6Hr_FasY5KX1A-76-3el4ujg/viewform!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'THE KING comment on joue avec Loren?') {
+message.reply('Pour jouer avec Lorenzo il faut remplire ce Questionnaire https://docs.google.com/forms/d/e/1FAIpQLSf0AduITS6vIDJQOo9rOo9RZv6Hr_FasY5KX1A-76-3el4ujg/viewform!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'THE KING comment on joue avec Loren') {
+message.reply('Pour jouer avec Lorenzo il faut remplire ce Questionnaire https://docs.google.com/forms/d/e/1FAIpQLSf0AduITS6vIDJQOo9rOo9RZv6Hr_FasY5KX1A-76-3el4ujg/viewform!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Chaine Youtube de lorenzo!') {
+message.reply('Voilat pour vous https://www.youtube.com/channel/UC6l6yLP6cVXeH4mHL6m0RxQ')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Chaine Youtube de loren') {
+message.reply('Voilat pour vous https://www.youtube.com/channel/UC6l6yLP6cVXeH4mHL6m0RxQ')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Chaine Youtube de lorenzo') {
+message.reply('Voilat pour vous https://www.youtube.com/channel/UC6l6yLP6cVXeH4mHL6m0RxQ')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Chaine youtube de lorenzo') {
+message.reply('Voilat pour vous https://www.youtube.com/channel/UC6l6yLP6cVXeH4mHL6m0RxQ')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'The king chaine youtube de lorenzo') {
+message.reply('Voilat pour vous https://www.youtube.com/channel/UC6l6yLP6cVXeH4mHL6m0RxQ')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'THE KING chaine youtube de lorenzo') {
+message.reply('Voilat pour vous https://www.youtube.com/channel/UC6l6yLP6cVXeH4mHL6m0RxQ')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'the king chaine youtube de lorenzo') {
+message.reply('Voilat pour vous https://www.youtube.com/channel/UC6l6yLP6cVXeH4mHL6m0RxQ')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Chaine Twitch de lorenzo!') {
+message.reply('Voilat pour vous https://www.twitch.tv/loren24_clash')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Chaine Twitch de loren') {
+message.reply('Voilat pour vous https://www.twitch.tv/loren24_clash')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Chaine Twitch de lorenzo') {
+message.reply('Voilat pour vous https://www.twitch.tv/loren24_clash')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Chaine twitch de lorenzo') {
+message.reply('Voilat pour vous https://www.twitch.tv/loren24_clash')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'The king chaine twitch de lorenzo') {
+message.reply('Voilat pour vous https://www.twitch.tv/loren24_clash')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'THE KING chaine twitch de lorenzo') {
+message.reply('Voilat pour vous https://www.twitch.tv/loren24_clash')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'the king chaine twitch de lorenzo') {
+message.reply('Voilat pour vous https://www.twitch.tv/loren24_clash')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Merci The king') {
+message.reply('A votre service!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Merci THE KING') {
+message.reply('A votre service!')
+}
+});
+bot.on('message',message => {
+  if (message.content === 'Merci the king') {
+message.reply('A votre service!')
+}
+});
